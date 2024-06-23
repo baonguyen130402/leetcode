b26c814b030c7e334d9e8884d1a6d6bfb6c8c8aa
Bao Nguyen <baonguyenx@Baos-Macbook-Air-M2.local>
2024-06-24 02:48:43 +0700

    update

---
+++ Merge-Sorted-Array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+const merge = (nums1, m, nums2, n) => {
+  for (let i = m, j = 0; j < n; i++, j++) {
+    nums1[i] = nums2[j];
+  }
+  nums1.sort((a, b) => a - b);
+
+  return nums1;
+};
+
+const r = merge([1, 2, 3], 3, [4, 5], 2);
+console.log(r);
